4535bc2324a0f55bc0d806d7e97f06c3616746d2
JUNZ1 <gigabayt@gmail.com>
2020-08-10 00:25:07 +0200

    Common data interface for storing input/output data for training is updated..
    This interface may be divided in to two interface but not decided yet..

--- IgodotNNInterfaces.cpp
+++ IgodotNNInterfaces.cpp
@@ -1,18 +1,38 @@
 #include "IgodotNNInterfaces.h"
 
+std::ostream& operator<<(std::ostream& os, const PoolRealArray& poolArray)
+{
+	os<<"Array :"<<std::endl;
+	for(int counter = 0; counter < poolArray.size() ; ++counter)
+	{
+		os<<poolArray.get(counter)<<" ";
+	}
+	os<<" "<<std::endl;
+	return os;
+}
+
 IGodotNNInterfaces::IGodotNNInterfaces(const IGodotNNInterfaces& copyFromThis)
 {
 	this->_inputVector = copyFromThis._inputVector;
 	this->_outputVector = copyFromThis._outputVector;
+	this->_multipleInputVector = copyFromThis._multipleInputVector;
 }
-
 
 IGodotNNInterfaces::IGodotNNInterfaces(IGodotNNInterfaces&& moveFromThis)
 {
 	this->_inputVector = moveFromThis._inputVector;
 	this->_outputVector = moveFromThis._outputVector;
+	this->_multipleInputVector = moveFromThis._multipleInputVector;
 	moveFromThis._inputVector.clear();
 	moveFromThis._outputVector.clear();
+	moveFromThis._multipleInputVector.clear();
+}
+
+void IGodotNNInterfaces::ClearAllData()
+{
+	_inputVector.clear();
+	_outputVector.clear();
+	_multipleInputVector.clear();
 }
 
 void IGodotNNInterfaces::SetInputs(const PoolRealArray& setToThis)
@@ -30,7 +50,6 @@ void IGodotNNInterfaces::SetInputs(const PoolRealArray& setToThis)
 		std::cout<<eachmember<<std::endl;
 	#endif
 }
-
 
 void IGodotNNInterfaces::SetOutputs(const PoolRealArray& setToThis)
 {
@@ -56,4 +75,33 @@ std::vector<double> IGodotNNInterfaces::GetInputVector() const noexcept
 std::vector<double> IGodotNNInterfaces::GetOutputVector()
 {
 	return _outputVector;
+}
+
+void IGodotNNInterfaces::InsertMultipleInput(const PoolRealArray &toThis)
+{
+	int incomingSize = toThis.size();
+	std::vector<double> tempinputVector;
+	tempinputVector.resize(incomingSize);
+	for(int indexer = 0; indexer < incomingSize; ++indexer)
+	{
+		tempinputVector[indexer] = toThis.get(indexer);
+	}
+	_multipleInputVector.push_back(tempinputVector);
+	#ifdef NNDEBUG
+	std::cout<<"Multiple Inputs Are Set"<<std::endl;
+	for(const auto& eachArray: _multipleInputVector)
+	{
+		std::cout<<"Array :"<<std::endl;
+		for(const auto& eachMember : eachArray)
+		{
+			std::cout<<eachMember<<" ";
+		}
+		std::cout<<" "<<std::endl;
+	}
+	#endif
+}
+
+std::vector<std::vector<double>> IGodotNNInterfaces::GetMultipleInput() const noexcept
+{
+	return _multipleInputVector;
 }
--- IgodotNNInterfaces.h
+++ IgodotNNInterfaces.h
@@ -2,10 +2,14 @@
 #define IGODOTNNINTERFACES_H
 #include <iostream>
 #include <vector>
+#include <iostream>
 #include "core/variant.h"
 #include "core/reference.h"
 #define NNDEBUG
 // Common Interface for All Neural Network Classes
+//This class may be seperated as two different class,based on Single Data and Multiple Data consept
+
+std::ostream& operator<<(std::ostream& os, const PoolRealArray& poolArray);
 
 class IGodotNNInterfaces : public Reference
 {
@@ -16,18 +20,29 @@ public:
 	virtual ~IGodotNNInterfaces(){}
 
 public: //Interfaces For Godot
+	void	ClearAllData(void);
+	void	InsertMultipleInput(const PoolRealArray& toThis);
 	void	SetInputs(const PoolRealArray&);
 	void	SetOutputs(const PoolRealArray&);
-	virtual void	StartTraining(void)=0;
-	virtual real_t	CalculateOutput(real_t)=0;
+
 
 public: //Interfaces For NoNGodot
+	std::vector<std::vector<double>> GetMultipleInput() const noexcept;
 	std::vector<double> GetInputVector() const noexcept;
 	std::vector<double> GetOutputVector();
 
+public:
+	virtual void	StartTraining(void)=0;
+	virtual real_t	CalculateOutput(real_t)=0;
+	virtual real_t	CalculateForMultipleOutput(PoolRealArray)=0;
+
 private:
+	std::vector<std::vector<double>> _multipleInputVector={};
 	std::vector<double> _inputVector={};
 	std::vector<double> _outputVector={};
+
 };
+
+
 
 #endif // IGODOTNNINTERFACES_H
